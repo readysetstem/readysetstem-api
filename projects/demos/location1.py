42d9c8480da621fa2b693dd115eedbbdd6150eaf
Brian Silverman <bri@readysetstem.com>
2016-06-26 13:13:56 -0400

    updated location projects

--- projects/demos/location1.py
+++ projects/demos/location1.py
@@ -9,27 +9,17 @@ mc = minecraft.Minecraft.create()
 SCALE = 25
 fb = FrameBuffer()
 
-count = 0
-FLASH_COUNT = 3
-flash_lit = True
 while True:
 	pos = mc.player.getTilePos()
 
 	x = round(pos.x/SCALE + (fb.width-1)/2)
-	x_out_of_bounds = not 0 <= x < fb.width
 	x = min(fb.width-1, max(0, x))
 
 	z = round(pos.z/SCALE + (fb.height-1)/2)
-	z_out_of_bounds = not 0 <= z < fb.height
 	z = min(fb.height-1, max(0, z))
 
 	fb.erase()
-	count += 1
-	if count > FLASH_COUNT:
-		flash_lit = not flash_lit
-		count = 0
-	if not x_out_of_bounds and not z_out_of_bounds or flash_lit:
-		fb.point(z, x)
+	fb.point(z, x)
 	fb.show()
 
 	time.sleep(0.01)
